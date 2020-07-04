8e7493a9afa1d00bbe857db424d1b291ffe866ff
tiagolagrecap <tiagolagrecap@gmail.com>
2020-07-04 07:59:22 -0700

    error

--- lalu/wsgi.py
+++ lalu/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lalu.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lalu.settings.dev')
 
 application = get_wsgi_application()
